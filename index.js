fb7efc7cf6abc32685155bc6a20a91b752b46820
GUILHERME LEONARDO PIRES COSTA <9170193@eseig.pt>
2018-03-08 16:20:43 +0000

    Ate alinea K

--- index.js
+++ index.js
@@ -39,3 +39,105 @@ function funcD() {
     }
     
 }
+
+//Alinea E
+function funcE() {
+    let peso = parseFloat(prompt("Escreva o seu peso em Kg: "))
+    let altura = parseFloat(prompt("Escreva o seu altura em M: "))
+    console.log("PESO: " + peso)
+    console.log("ALTURA: " + altura)
+
+    let imc = peso / (altura * altura)
+    console.log("IMC: " + imc)
+    if (imc < 18.5) {
+        console.log("Está abaixo do peso!!!")
+    }
+    else if (imc >= 18.5 && imc < 24.9) {
+        console.log("Está com um peso normal!")
+    }
+    else if (imc >= 24.9 && imc < 29.9) {
+        console.log("Está com sobrepeso!")
+    }
+    else if (imc >= 30 && imc < 34.9) {
+        console.log("Está com obesidade grau 1!")
+    }
+    else if (imc >= 35 && imc < 39.9) {
+        console.log("Está com obesidade grau 2!")
+    }
+    else{
+        console.log("Está com obesidade grua 3!")
+    }
+}
+
+//Alinea F
+function funcF() {
+    let string = prompt("Ecreva uma palavra")
+    let numero = prompt("Escreva um número")
+    for (let i=0; i<numero; i++) {
+        console.log(string)
+    }
+}
+
+//Alinea G
+function funcG() {
+    let valorA = parseInt(prompt("Escreva um número inteiro"))
+    let valorB = parseInt(prompt("Escreva outro número inteiro"))
+    for (let i = valorA; i<valorB; i++) {
+        console.log(i)
+    }
+}
+
+//Alinea H
+function funcH() {
+    let numero = parseInt(prompt("Escreva um número inteiro"))
+    for (let i = 1; i<=10; i++){
+        let resultado = numero*i
+        console.log(`${numero} x ${i} = ${resultado}`)
+    }
+}
+
+//Alinea I
+function funcI() {
+    let valorA = parseInt(prompt("Escreva o primeiro valor do intervalo"))
+    let valorB = parseInt(prompt("Escreva o segundo valor do intervalo"))
+    let multiplos = 0
+    for (let i = valorA; i<valorB; i++){
+        if(i % 3 == 0){
+           multiplos = multiplos+i
+        }
+    }
+    console.log(multiplos)
+}
+
+//Alinea J
+function funcJ() {
+    let numero = parseInt(prompt("Escreva um número: "))
+    let resultado
+    if(numero != 2){
+        for (let i = 2; i < numero; i++){
+            if(numero % i == 0){
+                resultado = "Não"
+                break
+            }
+            else{
+                resultado = "Sim"
+            }
+        }
+    }
+    else
+    {
+        resultado = "Sim"
+    }
+    console.log(resultado)
+}
+
+//Alinea K
+function funcK() {
+    let numero = parseInt(prompt("Escreva um numero inteiro: "))
+    let fatorial = 1
+    for(let i = 1; i <= numero; i++){
+        fatorial *= i
+    }
+    console.log(fatorial)
+
+}
