d08237104ea085bae29bd91d75572fa7466c1023
GUILHERME LEONARDO PIRES COSTA <9170193@eseig.pt>
2018-03-22 16:13:57 +0000

    Ficha_04 com alguns avanços

--- Ficha_04/ficha4.js
+++ Ficha_04/ficha4.js
@@ -3,16 +3,94 @@ function output(texto){
     let elemento = document.getElementById("output")
     elemento.innerHTML = texto
 }
-
+let student = {
+    name : "David Silva",
+    course : "POO",
+    grade: 12,
+    propriedades: function () {
+        return "Name: " + this.name + ", course:" + this.course + ", grade: " + this.grade
+    } 
+}
 //Alinea A
 function funcA() {
-    let student = {
-        name : "David Silva",
-        course : "POO",
-        grade: 12,
-        propriedades: function () {
-            "Name: " + this.name + ", course:" + this.course + ", grade: " + this.grade
-        } 
+    output(student.propriedades())
+    delete student.grade
+    output(student.propriedades())
+}
+
+//Alinea B
+function funcB() {
+    
+}
+
+//Alinea D
+
+let cores = ["vermelho", "verde", "preto", "amarelo", "azul"]
+class Carro {
+
+    constructor(marca, matricula, cor, deposito,tipo){
+
+        this.marca = marca
+        this.matricula = matricula
+        this.cor = cor
+        this.deposito = deposito
+        this.tipo = tipo
     }
-    console.log(student.propriedades())
+
+
+    get marca(){
+        return this._marca
+    }
+    set marca(newMarca){
+        this._marca = newMarca
+    }
+
+    get matricula(){
+        return this._matricula
+    }
+    set matricula(newMatricula){
+        this._matricula = newMatricula
+    }
+
+    get cor(){
+        return this._cor
+    }
+    set cor(newCor){
+
+        if (cores.indexOf(newCor != -1)) {
+            this._cor = newCor
+        }
+        else{
+            console.log("Cor Inválida")
+            this._cor = "sem cor"
+        }
+        this._cor = newCor
+    }
+
+    get deposito(){
+        return this._deposito
+    }
+    set deposito(newDeposito){
+        this._deposito = newDeposito
+    }
+
+
+    //Consumir
+    consumir(nKms){
+        let litrosConsumidos = (nKms*5) /100
+        if(litrosConsumidos > this.deposito){
+            console.log("Combustível insuficiente. Se quiser percorrer " + nKms + " terá de abastecer " + Math.abs(this.deposito - litrosConsumidos) + "L") 
+        }
+        else{
+            this.deposito -= litrosConsumidos                        
+        }
+    }
+}
+
+let meuCarro1 = new Carro("Ford", "91-GH-15", "verde", "40", "Gasolina")
+let meuCarro2 = new Carro("Fiat", "15-FD-15", "branco", "50", "Gasóleo")
+
+function funcD(){
+    
+    meuCarro1.consumir(801)
 }
