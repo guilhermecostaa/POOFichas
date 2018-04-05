037e51323247380dbfb125a01eccf0d7d1b83e48
Guilherme Costa <guilherme.costaa19@gmail.com>
2018-04-05 20:57:08 +0100

    falta ultima alinea

--- Ficha_04/ficha4.js
+++ Ficha_04/ficha4.js
@@ -1,4 +1,4 @@
-function output(texto){
+function output(texto) {
     console.log(texto)
     let elemento = document.getElementById("output")
     elemento.innerHTML = texto
@@ -6,12 +6,12 @@ function output(texto){
 
 //Alinea A
 let student = {
-    name : "David Silva",
-    course : "POO",
+    name: "David Silva",
+    course: "POO",
     grade: 12,
     propriedades: function () {
         return "Name: " + this.name + ", course:" + this.course + ", grade: " + this.grade
-    } 
+    }
 }
 
 function funcA() {
@@ -23,34 +23,34 @@ function funcA() {
 
 
 //Alinea B
-class Pessoa{
+class Pessoa {
 
-    constructor(name, grade){
+    constructor(name, grade) {
         this.name = name
         this.grade = grade
     }
 
     //Propriedade name
-    get name(){
+    get name() {
         return this._name
     }
-    set name(newName){
+    set name(newName) {
         this._name = newName
 
     }
 
     //Propriedade grade
-    get grade(){
+    get grade() {
         return this._grade
     }
-    set grade(newGrade){
+    set grade(newGrade) {
         this._grade = newGrade
     }
 
 }
 
 
-function funcB(){
+function funcB() {
     let grades = [
         {
             name: 'Rui',
@@ -65,14 +65,14 @@ function funcB(){
             grade: 14
         }]
     let numeroObjetos = parseInt(prompt("Quantos objetos pretende adicionar?"))
-    for(let i = 0; i<numeroObjetos; i++){
+    for (let i = 0; i < numeroObjetos; i++) {
         name = prompt("Escreva o nome da pessoa")
         grade = prompt("Escreva a nota dessa pessoa")
-        let object = new Pessoa (name,grade)
+        let object = new Pessoa(name, grade)
         grades.push(object)
     }
-    for(let i = 0; i < grades.length; i++){
-       console.log(grades[i])
+    for (let i = 0; i < grades.length; i++) {
+        console.log(grades[i])
     }
 }
 
@@ -83,7 +83,7 @@ function funcB(){
 let cores = ["vermelho", "verde", "preto", "amarelo", "azul"]
 class Carro {
 
-    constructor(marca, matricula, cor, deposito,tipo){
+    constructor(marca, matricula, cor, deposito, tipo) {
 
         this.marca = marca
         this.matricula = matricula
@@ -93,51 +93,51 @@ class Carro {
     }
 
 
-    get marca(){
+    get marca() {
         return this._marca
     }
-    set marca(newMarca){
+    set marca(newMarca) {
         this._marca = newMarca
     }
 
-    get matricula(){
+    get matricula() {
         return this._matricula
     }
-    set matricula(newMatricula){
+    set matricula(newMatricula) {
         this._matricula = newMatricula
     }
 
-    get cor(){
+    get cor() {
         return this._cor
     }
-    set cor(newCor){
+    set cor(newCor) {
 
         if (cores.indexOf(newCor != -1)) {
             this._cor = newCor
         }
-        else{
+        else {
             console.log("Cor Inválida")
             this._cor = "sem cor"
         }
         this._cor = newCor
     }
 
-    get deposito(){
+    get deposito() {
         return this._deposito
     }
-    set deposito(newDeposito){
+    set deposito(newDeposito) {
         this._deposito = newDeposito
     }
 
 
     //Consumir
-    consumir(nKms){
-        let litrosConsumidos = (nKms*5) /100
-        if(litrosConsumidos > this.deposito){
-            console.log("Combustível insuficiente. Se quiser percorrer " + nKms + " terá de abastecer " + Math.abs(this.deposito - litrosConsumidos) + "L") 
+    consumir(nKms) {
+        let litrosConsumidos = (nKms * 5) / 100
+        if (litrosConsumidos > this.deposito) {
+            console.log("Combustível insuficiente. Se quiser percorrer " + nKms + " terá de abastecer " + Math.abs(this.deposito - litrosConsumidos) + "L")
         }
-        else{
-            this.deposito -= litrosConsumidos                        
+        else {
+            this.deposito -= litrosConsumidos
         }
     }
 }
@@ -145,74 +145,75 @@ class Carro {
 let meuCarro1 = new Carro("Ford", "91-GH-15", "verde", "40", "Gasolina")
 let meuCarro2 = new Carro("Fiat", "15-FD-15", "branco", "50", "Gasóleo")
 
-function funcC(){
+function funcC() {
     meuCarro1.consumir(801)
 }
 
 //Alinea D
 class Cilindro {
-    constructor(raio, altura){
+    constructor(raio, altura) {
         this.raio = raio
         this.altura = altura
     }
 
     //Propriedade raio
-    get raio (){
+    get raio() {
         return this._raio
     }
-    set raio (novoRaio){
+    set raio(novoRaio) {
         this._raio = novoRaio
     }
 
     //Propriedade altura
-    get altura (){
+    get altura() {
         return this._altura
     }
-    set altura (novoAltura){
+    set altura(novoAltura) {
         this._altura = novoAltura
     }
 
     //Formula calcular o volume
-    volume(){
-        let calcularVolume = 3.14*this._raio*this._raio*this._altura
+    volume() {
+        return (3.14 * this.raio * this.raio * this.altura).toFixed(4)
     }
 }
 
 function funcD() {
-    let cyl = new Cilindro(7,4)
-    console.log('volume =', cyl.volume().toFixed(4))
+    let cyl = new Cilindro(7, 4)
+    console.log('volume =', cyl.volume())
 }
 
 //Alinea E
-class Circulo{
-    constructor(raio){
+class Circulo {
+    constructor(raio) {
         this.raio = raio
     }
 
     //Propriedade raio
-    get raio (){
+    get raio() {
         return this._raio
     }
-    set raio (novoRaio){
+    set raio(novoRaio) {
         this._raio = novoRaio
     }
 
     //Formula Area
-    area(){
-        let calcularArea = 3.14*this._raio*this._raio
+    area() {
+        return (3.14 * this.raio * this.raio).toFixed(2)
+
     }
 
     //Formula Perimetro
-    perimetro(){
-        let calcularPerimetro = (2*3.14)*this._raio
+    perimetro() {
+        return ((2 * 3.14) * this.raio).toFixed(2)
     }
 }
 
 function funcE() {
-        let raiodocirculo = parseInt(prompt("Escreva o raio do circulo"))
-        let c = new Circulo(raiodocirculo)
-        console.log('Area =', c.area().toFixed(2))
-        console.log('perimeter =', c.perimetro().toFixed(2)) 
+    let raiodocirculo = parseInt(prompt("Escreva o raio do circulo"))
+    let c = new Circulo(raiodocirculo)
+    console.log('Area =', c.area())
+    console.log('perimeter =', c.perimetro())
 }
 
 //Alinea F
