0edc3e7dd393ce70a29536d70f7d74ac2ed53135
Guilherme Costa <guilherme.costaa19@gmail.com>
2018-04-20 15:57:41 +0100

    Ficha_09

---
+++ Ficha_09/ex_01.js
@@ -0,0 +1,41 @@
+//Criacao da Classe para o utilizador
+class Utilizador{
+    constructor(nome, email, password){
+        this.nome = nome
+        this.email = email
+        this.password = password
+    }
+
+    //Propriedade Nome
+    get nome(){
+        return this._nome
+    }
+    set nome(novoNome){
+        this._nome = novoNome
+    }
+
+    
+    //Propriedade Email
+    get email(){
+        return this._email
+    }
+    set email(novoEmail){
+        this._email = novoEmail
+    }
+
+    //Propriedade Password
+    get password(){
+        return this._password
+    }
+    set password(novoPassword){
+        this._password = novoPassword
+    }
+}
+
+
+window.onload = function(){
+    let logout = document.getElementById("inputLogout")
+    logout.style.display = "none"
+    let bemVindo = document.getElementById("bemVindo")
+    bemVindo.style.display = "none"
+}
