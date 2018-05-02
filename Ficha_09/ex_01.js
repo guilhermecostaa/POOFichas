0899b62bf15ac90b8f13d6bdbe158cddbbf80c6d
Guilherme Costa <guilherme.costaa19@gmail.com>
2018-05-02 14:41:49 +0100

    Ficha9

--- Ficha_09/ex_01.js
+++ Ficha_09/ex_01.js
@@ -31,10 +31,44 @@ class Utilizador{
     }
 }
 
+let utilizadores = []
 
 window.onload = function(){
+    //Variaveis Login
+    let formLogin = document.getElementById("formLogin")
+    let emailLogin = document.getElementById("inputEmailLogin")
+    let passwordLogin = document.getElementById("inputPasswordLogin")
+
+    //Variaveis Registo
+    let formRegisto = document.getElementById("formRegisto")
+    let nome = document.getElementById("inputUserRegisto")
+    let password = document.getElementById("inputPasswordRegsito")
+    let email = document.getElementById("inputEmailRegisto")
+    let confirmarPassword = document.getElementById("inputConfrimarPasswordRegisto")
+    let validacao = false
+    let mensagem = "" 
+
+    //Variaveis Logout
     let logout = document.getElementById("inputLogout")
     logout.style.display = "none"
     let bemVindo = document.getElementById("bemVindo")
     bemVindo.style.display = "none"
+
 }
+
+formRegisto.addEventListener("submit", function(event){
+        
+    if (password == confirmarPassword) {
+        validacao = true
+    }
+
+    if (validacao) {
+        let newUtilizador = new Utilizador(nome.value, email.value, password.value)
+        utilizadores.push(newUtilizador)
+    } else{
+        event.preventDefault();
+    }
+    console.log(utilizadores.length)
+})
+
+
